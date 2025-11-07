2981143ab2b44125ad56f98165aa5c60729f1f04
nick-abramov <nickabramov2002@gmail.com>
2025-11-08 01:28:17 +0500

    исправленно дз

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,23 @@
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println(" Задача 1 ");
-        checkYears();
+        int year = 2204;
+        checkYears(year);
 
         System.out.println(" Задача 2 ");
-        checkOC();
+        int clientOS = 0;
+        int clientDeviceYear = 2014;
+        checkOC(clientOS,clientDeviceYear);
 
         System.out.println(" Задача 3 ");
-        checkDistance();
+        int deliveryDistance = 95;
+        checkDistance(deliveryDistance);
     }
-    public static void checkYears() {
-        int year = 2204;
+    public static void checkYears(int year) {
+
         if (year < 1584) {
             System.out.println(year + " год не является високосным");
         } else if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
@@ -22,26 +27,26 @@ public class Main {
         }
     }
 
-    public static void checkOC() {
-        int clientOS = 0;
-        int clientDeviceYear = 2014;
+    public static void checkOC( int clientOS, int clientDeviceYear) {
 
-        if (clientOS == 0 && clientDeviceYear >= 2015) {
+        int currentYear = LocalDate.now().getYear();
+        if (clientOS == 0 && clientDeviceYear >= currentYear) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+        } else if (clientOS == 0 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегчённую версию приложения для iOS по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+        } else if (clientOS == 1 && clientDeviceYear >= currentYear) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+        } else if (clientOS == 1 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегчённую версию приложения для Android по ссылке");
         } else {
             System.out.println("Выберете корректную OS из списка");
         }
 
+
     }
 
-    public static void checkDistance() {
-        int deliveryDistance = 95;
+    public static void checkDistance(int deliveryDistance) {
+
         if (deliveryDistance <= 20) {
             System.out.println("Потребуется дней: 1 ");
         } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
